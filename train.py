a5aeb531eae620830dd90271268f43292e465302
minhle24-cd <minhleit24@gmail.com>
2026-05-02 19:23:58 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,13 @@
 from ultralytics import YOLO
 
-# 1. Khởi tạo mô hình YOLOv8 (dùng file weight bạn đang có)
+# 1. Khởi tạo mô hình YOLOv
 model = YOLO('yolov8n.pt')
 
 # 2. Chạy lệnh train
-# Bạn có thể điều chỉnh các thông số này theo ý muốn
 results = model.train(
-    data='data.yaml',   # File cấu hình dataset của bạn
-    epochs=100,         # Số vòng lặp train
-    imgsz=640,          # Kích thước hình ảnh
-    device=0,           # Chạy bằng GPU (nếu máy có GPU)
-    name='priority_vehicle_model' # Tên thư mục lưu kết quả
+    data='data.yaml',   
+    epochs=100,         
+    imgsz=640,          
+    device=0,          
+    name='priority_vehicle_model' 
 )
